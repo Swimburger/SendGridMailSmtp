2fea330014de6a75bb1cd684f7868ffef8df232a
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-02-17 22:01:05 -0500

    initial commit

---
+++ SendGridMail.MailKit/Program.cs
@@ -0,0 +1,34 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+using var message = new MimeMessage();
+message.From.Add(new MailboxAddress(
+    "[REPLACE WITH YOUR NAME]", 
+    "[REPLACE WITH YOUR EMAIL]"
+));
+message.To.Add(new MailboxAddress(
+    "[REPLACE WITH DESIRED TO NAME]", 
+    "[REPLACE WITH DESIRED TO EMAIL]"
+));
+message.Subject = "Sending with Twilio SendGrid is Fun";
+var bodyBuilder = new BodyBuilder
+{
+    TextBody = "and easy to do anywhere, especially with C#",
+    HtmlBody = "and easy to do anywhere, <b>especially with C#</b>"
+};
+message.Body = bodyBuilder.ToMessageBody();
+
+using var client = new SmtpClient();
+// SecureSocketOptions.StartTls force a secure connection over TLS
+await client.ConnectAsync("smtp.sendgrid.net", 587, SecureSocketOptions.StartTls);
+await client.AuthenticateAsync(
+    userName: "apikey", // the userName is the exact string "apikey" and not the API key itself.
+    password: Environment.GetEnvironmentVariable("SendGridApiKey") // password is the API key
+);
+
+Console.WriteLine("Sending email");
+await client.SendAsync(message);
+Console.WriteLine("Email sent");
+
+await client.DisconnectAsync(true);
---
+++ SendGridMail.Net.Mail/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+
+using var message = new MailMessage();
+message.From = new MailAddress(
+    "[REPLACE WITH YOUR EMAIL]",
+    "[REPLACE WITH YOUR NAME]"
+);
+message.To.Add(new MailAddress(
+    "[REPLACE WITH DESIRED TO EMAIL]",
+    "[REPLACE WITH DESIRED TO NAME]"
+));
+message.Subject = "Sending with Twilio SendGrid is Fun";
+var textBody = "and easy to do anywhere, especially with C#";
+var htmlBody = "and easy to do anywhere, <b>especially with C#</b>";
+
+message.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(
+    textBody, null, MediaTypeNames.Text.Plain)
+);
+message.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(
+    htmlBody, null, MediaTypeNames.Text.Html)
+);
+
+// if you only want to send HTML email, simply use this
+// message.Body = htmlBody;
+// message.IsBodyHtml = true;
+
+using var client = new SmtpClient(host: "smtp.sendgrid.net", port: 587);
+client.EnableSsl = true; // IMPORTANT to send mails securely
+client.Credentials = new NetworkCredential(
+    userName: "apikey", // the userName is the exact string "apikey" and not the API key itself.
+    password: Environment.GetEnvironmentVariable("SendGridApiKey") // password is the API key
+);
+
+Console.WriteLine("Sending email");
+await client.SendMailAsync(message);
+Console.WriteLine("Email sent");
